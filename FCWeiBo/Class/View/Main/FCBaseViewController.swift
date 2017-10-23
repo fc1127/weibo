34538b95e0fbb5f3d9babbb2a09df6a69e6041a8
自由自在 <1462384560@qq.com>
2017-10-23 11:31:57 +0800

    gfdsga

--- FCWeiBo/Class/View/Main/FCBaseViewController.swift
+++ FCWeiBo/Class/View/Main/FCBaseViewController.swift
@@ -28,12 +28,7 @@ extension FCBaseViewController{
 
     func setupUI(){
 //        调整视图
-        tableview = UITableView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height), style: UITableViewStyle.init(rawValue: 0)!)
-        automaticallyAdjustsScrollViewInsets=false
-  tableview.contentInset=UIEdgeInsetsMake(navigationController?.navigationBar.bounds.height ?? 64, 0, tabBarController?.tabBar.bounds.height ?? 49, 0)
-        tableview.backgroundColor=UIColor.red
-        tableview.delegate=self
-        tableview.dataSource=self
+
         view.addSubview(tableview)
     }
 }
